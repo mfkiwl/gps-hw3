473a4e7cf7ea0f70c26a6c1457bcf475bba36b0a
Tanner Koza <jtk0018@auburn.edu>
2022-03-03 17:15:13 -0600

    problem 7 checkpoint 1

--- hw3.m
+++ hw3.m
@@ -283,9 +283,46 @@ ylabel('Bit Value')
 
 clearvars
 
+%% Problem 7
 
+codeL = 1023;
 
+CA4 = genCA(4, codeL);
+CA7 = genCA(7, codeL);
 
+idx4 = find(CA4 == 0);
+idx7 = find(CA7 == 0);
 
+CA4(idx4) = -1;
+CA7(idx7) = -1;
 
+figure
+histogram(CA4)
+title('PRN 4 C/A Histogram')
 
+figure
+histogram(CA7)
+title('PRN 7 C/A Histogram')
+
+figure
+periodogram(CA4)
+title('PRN 4 C/A PSD Estimate')
+
+figure
+periodogram(CA7)
+title('PRN 7 C/A PSD Estimate')
+
+figure
+autocorr(CA4, 'NumLags', codeL-1)
+title('PRN 4 C/A Autocorrelation')
+xlim([0 codeL-1])
+
+figure
+autocorr(CA7, 'NumLags', codeL-1)
+title('PRN 7 C/A Autocorrelation')
+xlim([0 codeL-1])
+
+figure
+crosscorr(CA7, 'NumLags', codeL-1)
+title('PRN 4 & 7 C/A Cross Correlation')
+xlim([-codeL codeL])
