a7c9be5998f6eb579ea0f07520e6d1c30ff1ffb6
Tanner Koza <jtk0018@auburn.edu>
2022-03-03 18:15:25 -0600

    problem 8 checkpoint 1

--- hw3.m
+++ hw3.m
@@ -40,7 +40,7 @@ clearvars
 
 numSims = 1000;
 
-dt = 0.1; % NOTE: Only matches anlytical for a dt of 1.
+dt = 0.1; 
 t_end = 6000; % end time (s)
 t = 0:dt:t_end;
 Ts = length(t);
@@ -326,3 +326,37 @@ figure
 crosscorr(CA7, 'NumLags', codeL-1)
 title('PRN 4 & 7 C/A Cross Correlation')
 xlim([-codeL codeL])
+
+clearvars
+
+%% Problem 8 
+
+codeL = 1023;
+
+CA4 = genCA(4, codeL);
+CA7 = genCA(7, codeL);
+
+idx4 = find(CA4 == 0);
+idx7 = find(CA7 == 0);
+
+CA4(idx4) = -1;
+CA7(idx7) = -1;
+
+CA4 = reshape(repmat(CA4',10,1),1,[]);
+CA7 = reshape(repmat(CA7',10,1),1,[]);
+
+fL1 = 1575.42e6; % L1 frequency (Hz)
+
+fs = fL1*10;
+
+dt = 1/fs;
+
+t = 0:dt:dt*(length(CA4)-1);
+
+L1car = sin((fL1*2*pi).*t);
+
+L1sig4 = L1car .* CA4;
+
+L1sig7 = L1car .* CA7;
+
+% TODO: PLOT & ADJUST XLIM 
