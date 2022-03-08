6777a7e7e0df71426185e9824c241e9ec6539922
Tanner Koza <jtk0018@auburn.edu>
2022-03-08 15:08:49 -0600

    problem 2 checkpoint 1

--- problems/p1.m
+++ problems/p1.m
@@ -115,70 +115,70 @@ clearvars
 
 %% Part C
 
-% numSims = 1000;
-% 
-% sigma1 = 0.1;
-% sigma2 = 0.01;
-% 
-% dt = 1;
-% t_end = 6000;
-% t = 0:dt:t_end;
-% Ts = length(t);
-% tau1 = 1;
-% tau2 = 100;
-% 
-% A1 = 1 - (dt/tau1);
-% A2 = 1 - (dt/tau2);
-% 
-% sigma_x1 = sigma1 * dt * sqrt( (A1^2*t - 1)/(A1^2 - 1) );
-% sigma_x2 = sigma1 * dt * sqrt( (A2^2*t - 1)/(A2^2 - 1) );
-% sigma_x3 = sigma2 * dt * sqrt( (A1^2*t - 1)/(A1^2 - 1) );
-% sigma_x4 = sigma2 * dt * sqrt( (A2^2*t - 1)/(A2^2 - 1) );
-% 
-% x1 = 0;
-% x2 = 0;
-% x3 = 0;
-% x4 = 0;
-% 
-% x1_log = zeros(Ts, numSims);
-% x2_log = zeros(Ts, numSims);
-% x3_log = zeros(Ts, numSims);
-% x4_log = zeros(Ts, numSims);
-% 
-% for i = 1:numSims
-% 
-%     noise1 = sigma1 * randn(Ts,1); % unit/s
-%     noise2 = sigma2 * randn(Ts,1); % unit/s
-% 
-%     for j = 1:Ts
-%         
-%         x_dot1 = -(1/tau1) * x1 + noise1(j);
-%         x1 = x1 + x_dot1 * dt;
-%     
-%         x_dot2 = -(1/tau2) * x2 + noise1(j);
-%         x2 = x2 + x_dot2 * dt;
-% 
-%         x_dot3 = -(1/tau1) * x1 + noise2(j);
-%         x3 = x3 + x_dot3 * dt;
-%     
-%         x_dot4 = -(1/tau2) * x2 + noise2(j);
-%         x4 = x4 + x_dot4 * dt;
-% 
-%         x1_log(j,i) = x1;
-%         x2_log(j,i) = x2;
-%         x3_log(j,i) = x3;
-%         x4_log(j,i) = x4;
-%         
-%     end
-% 
-%     x1 = 0;
-%     x2 = 0;
-%     x3 = 0;
-%     x4 = 0;
-% 
-% end
-% 
-% 
+numSims = 1000;
+
+sigma1 = 0.1;
+sigma2 = 0.01;
+
+dt = 0.1;
+t_end = 6000;
+t = 0:dt:t_end;
+Ts = length(t);
+tau1 = 1;
+tau2 = 100;
+
+A1 = 1 - (dt/tau1);
+A2 = 1 - (dt/tau2);
+
+sigma_x1 = sigma1 * dt * sqrt( (A1.^(2*t) - 1) ./ (A1.^2 - 1) );
+sigma_x2 = sigma1 * dt * sqrt( (A2.^(2*t) - 1) ./ (A2.^2 - 1) );
+sigma_x3 = sigma2 * dt * sqrt( (A1.^(2*t) - 1) ./ (A1.^2 - 1) );
+sigma_x4 = sigma2 * dt * sqrt( (A2.^(2*t) - 1) ./ (A2.^2 - 1) );
+
+x1 = 0;
+x2 = 0;
+x3 = 0;
+x4 = 0;
+
+x1_log = zeros(Ts, numSims);
+x2_log = zeros(Ts, numSims);
+x3_log = zeros(Ts, numSims);
+x4_log = zeros(Ts, numSims);
+
+for i = 1:numSims
+
+    noise1 = sigma1 * randn(Ts,1); % unit/s
+    noise2 = sigma2 * randn(Ts,1); % unit/s
+
+    for j = 1:Ts
+        
+        x_dot1 = -(1/tau1) * x1 + noise1(j);
+        x1 = x1 + x_dot1 * dt;
+    
+        x_dot2 = -(1/tau2) * x2 + noise1(j);
+        x2 = x2 + x_dot2 * dt;
+
+        x_dot3 = -(1/tau1) * x1 + noise2(j);
+        x3 = x3 + x_dot3 * dt;
+    
+        x_dot4 = -(1/tau2) * x2 + noise2(j);
+        x4 = x4 + x_dot4 * dt;
+
+        x1_log(j,i) = x1;
+        x2_log(j,i) = x2;
+        x3_log(j,i) = x3;
+        x4_log(j,i) = x4;
+        
+    end
+
+    x1 = 0;
+    x2 = 0;
+    x3 = 0;
+    x4 = 0;
+
+end
+
+
 % figure
 % plot(t,an_std_wn1)
 % hold on
---
+++ problems/p2.m
@@ -0,0 +1,24 @@
+%% Fundamentals of GPS - HW3 - Problem 2
+
+clear
+clc
+
+%% Solution
+
+% Pseudorange Accuracy
+sigma = 0.5;
+
+% GPS L-Band Frequencies (MHz)
+L1 = 1575.42;
+L2 = 1227.60;
+L5 = 1176.45;
+
+L1L5(1) = ( L1^2 / (L1^2 - L5^2) );
+L1L5(2) = ( L5^2 / (L1^2 - L5^2) );
+L2L5(1) = ( L2^2 / (L2^2 - L5^2) );
+L2L5(2) = ( L5^2 / (L2^2 - L5^2) );
+
+sigma_L1L5 = sqrt(L1L5(1) * sigma^2 + L1L5(2) * sigma^2);
+sigma_L2L5 = sqrt(L2L5(1) * sigma^2 + L2L5(2) * sigma^2);
+
+% L!-L5 provides the best estimate.
